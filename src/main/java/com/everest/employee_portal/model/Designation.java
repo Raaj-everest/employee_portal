f95b077be408f81067369bd9c045329a8545b9e2
rajesh <raaj.everest@gmail.com>
2022-02-02 16:46:43 +0530

    addded enums for UserType and Designation

---
+++ src/main/java/com/everest/employee_portal/model/Designation.java
@@ -0,0 +1,5 @@
+package com.everest.employee_portal.model;
+
+public enum Designation {
+    MANAGER, TEAM_LEAD, CRAFT_PERSON, CEO
+}
---
+++ src/main/java/com/everest/employee_portal/model/UserType.java
@@ -0,0 +1,5 @@
+package com.everest.employee_portal.model;
+
+public enum UserType {
+    ADMIN, EMPLOYEE
+}
