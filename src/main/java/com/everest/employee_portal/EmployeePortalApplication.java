6a5160eb8685c6328c92882694c629e808b14382
rajesh <raaj.everest@gmail.com>
2022-02-08 23:20:42 +0530

    connection with DB established succesfully

--- src/main/java/com/everest/employee_portal/EmployeePortalApplication.java
+++ src/main/java/com/everest/employee_portal/EmployeePortalApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmployeePortalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeePortalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeePortalApplication.class, args);
+    }
 
 }
