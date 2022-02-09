e5957190fb4125957cc85a6a241542c4d60f95fb
rajesh <raaj.everest@gmail.com>
2022-02-09 16:43:01 +0530

    added implemtation for getting all employees

--- src/main/java/com/everest/employee_portal/services/EmployeeService.java
+++ src/main/java/com/everest/employee_portal/services/EmployeeService.java
@@ -3,15 +3,20 @@ package com.everest.employee_portal.services;
 import com.everest.employee_portal.entities.Employee;
 import com.everest.employee_portal.repositories.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class EmployeeService {
 
-    private EmployeeRepository employeeRepository;
+    private final EmployeeRepository employeeRepository;
+
+    public List<Employee> getAll(){
+        return employeeRepository.findAll();
+    }
 
 
 }
